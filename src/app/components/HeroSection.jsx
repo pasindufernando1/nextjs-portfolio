c222ee2fbee75b5f05501bf3f6728b9aa7223494
Pasindu Fernando <84668279+pasindufernando1@users.noreply.github.com>
2024-11-12 13:48:46 +0530

    CV and Transcript added

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -53,11 +53,19 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="/cv/CV.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
+              </span>
+            </Link>
+            <Link
+              href="/cv/Transcript.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3 ml-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download Current Transcript
               </span>
             </Link>
           </div>
