37334db5cacc89515785bbd671c6ef062da710bb
Pasindu Fernando <84668279+pasindufernando1@users.noreply.github.com>
2024-11-12 13:55:43 +0530

    Achievements updated

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -12,7 +12,7 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
   {
     metric: "Projects",
-    value: "10+",
+    value: "10",
     postfix: "+",
   },
   {
@@ -30,7 +30,8 @@ const achievementsList = [
   },
   {
     metric: "Volunteering",
-    value: "4+",
+    value: "4",
+    postfix: "+",
   },
 ];
 
@@ -38,33 +39,33 @@ const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
       <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
-        {achievementsList.map((achievement, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
-            >
-              <h2 className="text-white text-4xl font-bold flex flex-row">
-                {achievement.prefix}
+        {achievementsList.map((achievement, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+          >
+            <h2 className="text-white text-4xl font-bold flex flex-row">
+              {achievement.prefix}
+              {achievement.metric === "Current GPA" ? (
+                <span className="text-white text-4xl font-bold">{achievement.value}</span>
+              ) : (
                 <AnimatedNumbers
                   includeComma
                   animateToNumber={parseInt(achievement.value)}
                   locale="en-US"
                   className="text-white text-4xl font-bold"
-                  configs={(_, index) => {
-                    return {
-                      mass: 1,
-                      friction: 100,
-                      tensions: 140 * (index + 1),
-                    };
-                  }}
+                  configs={(_, index) => ({
+                    mass: 1,
+                    friction: 100,
+                    tensions: 140 * (index + 1),
+                  })}
                 />
-                {achievement.postfix}
-              </h2>
-              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-            </div>
-          );
-        })}
+              )}
+              {achievement.postfix}
+            </h2>
+            <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
